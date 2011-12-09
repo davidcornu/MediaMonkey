2272256360ed7d193a384c6543aee69cb2c3137b
David Cornu <davidjcornu@gmail.com>
2011-12-08 22:19:17 -0500

    Added /test route with a form to simplify things

--- lib/media_monkey/server.rb
+++ lib/media_monkey/server.rb
@@ -1,25 +1,55 @@
 require "sinatra/base"
+require "yaml"
+require "./item.rb"
 
 module MediaMonkey
   class Server < Sinatra::Base
-
-    # Application directory
+    
     dir = File.dirname(File.expand_path(__FILE__))
 
-    configure do
-      # Load MediaMonkey YAML configuration file
-      set :configuration, YAML.load_file("#{dir}config_templates/media_monkey.yml")
+    set :configuration, YAML.load_file("#{dir}/config_templates/media_monkey.yml")
+
+    # Create base directory if it doesn't exist
+    FileUtils.mkdir_p(configuration['base_directory'])
+    
+    get "/test" do
+      return <<-EOF
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>Test Page</title>
+          </head>
+          <body>
+            <form action="/profiles/1" method="POST" enctype="multipart/form-data">
+              <input type='file' name='file'/>
+              <input type='submit'/>
+            </form>
+          </body>
+        </html>
+      EOF
     end
     
-    post "/:resource/:id(/:type)" do
-      # params[:file][:filename]
-      # FileUtils.mkdir_p()
-      # FileUtils.cp(params[:document][:file][:tempfile].path, somewhere)
+    post "/:resource/:id/?:type?" do
+      @item = Item.new
+      @item.resource = params[:resource]
+      @item.resource_id = params[:id]
+      @item.file_path = params[:file][:tempfile].path
+      @item.file_name = params[:file][:filename]
+      @item.type = params[:type]
+        
+      if @item.process
+        "Success"
+      else
+        raise "Error processing item."
+      end
     end
     
     delete "/:resource/:id/:name" do
       
     end
+    
+    # Start the server if ruby file executed directly
+    run! if app_file == $0
     
   end
 end
